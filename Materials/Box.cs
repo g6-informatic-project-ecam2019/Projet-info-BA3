9a551918b5ecadd2347fcafc9641d38a39b9b486
NaberTimothy <timnaber96@gmail.com>
2019-02-26 15:12:23 +0100

    Update Box.cs

--- Materials/Box.cs
+++ Materials/Box.cs
@@ -11,6 +11,7 @@ namespace Materials
         private Dictionary<string, Object> dicCleat;
         private Dictionary<string, Object> dicBreadthGD;
         private Dictionary<string, Object> dicBreadthAR;
+        private Dictionary<string, Object> dicBreadthAV;
         private Dictionary<string, Object> dicPanelGD;
         private Dictionary<string, Object> dicPanelAR;
         private Dictionary<string, Object> dicDoor;
@@ -18,37 +19,49 @@ namespace Materials
         private int height;
         private int depth;
         private int width;
+        private double price;
 
-        public Box(Cleat cleat, Breadth breadthGD, Breadth breadthAR, Panel panelGD, Panel panelAR, Door door) /*builder*/
+        public Box(Cleat cleat, Breadth breadthGD, Breadth breadthAR, Breadth breadthAV, Panel panelGD, Panel panelAR, Door door) /*builder*/
         {
             this.dicCleat = cleat.GetDescription();
             this.dicBreadthGD = breadthGD.GetDescription();
             this.dicBreadthAR = breadthAR.GetDescription();
+            this.dicBreadthAV = breadthAV.GetDescription();
             this.dicPanelGD = panelGD.GetDescription();
             this.dicPanelAR = panelAR.GetDescription();
             this.dicDoor = door.GetDescription();
+
             this.height = Convert.ToInt32(dicCleat["length"])+2*20; /*The height of a box = at the height of the cleats + 2 * the height of a breadth (2cm)*/
             this.depth = Convert.ToInt32(dicBreadthGD["length"]); /*the depth is the length of the breadthGD*/
             this.width = Convert.ToInt32(dicBreadthAR["length"]); /*the depth is the length of the breadthAR*/
             this.pannelsColor = Convert.ToString(dicPanelGD["color"]); /*Panel color*/
+            this.price = Convert.ToDouble(dicCleat["price"]) * 4 + Convert.ToDouble(dicPanelGD["price"]) * 2 +
+                         Convert.ToDouble(dicPanelAR["price"]) * 1 + Convert.ToDouble(dicDoor["price"]) * 1 +
+                         Convert.ToDouble(dicBreadthGD["price"]) * 2 + Convert.ToDouble(dicBreadthAR["price"]) * 1 +
+                         Convert.ToDouble(dicBreadthAV["price"]) * 1;
         }
         public void SetColor(string color)
         {
             this.pannelsColor = color;
             dicPanelAR["color"] = color;
-            dicPanelGD["color"] = color;
+            dicPanelGD["color"] = color; /*Demander a bernard l'interet*/
         }
         public void SetDimensions(int Dimention)
         {
-
+            /*Demander à bernard a quoi ça sert dans son diagramme*/
         }
         public double GetPrice()
         {
-            return Convert.ToDouble(dicCleat["price"]) + Convert.ToDouble(dicPanelGD["price"]) + Convert.ToDouble(dicPanelAR["price"]) + Convert.ToDouble(dicDoor["price"]) + Convert.ToDouble(dicBreadthGD["price"]) + Convert.ToDouble(dicBreadthAR["price"]);
+            return price; /*Demander a bernard l'interet ? */
         }
-        public object GetDescription()
+        public Dictionary<string, Object> GetDescription()
         {
-            return 0;
+            Dictionary<string, Object> Description = new Dictionary<string, Object>();
+            Description.Add("height", height);
+            Description.Add("depth", depth);
+            Description.Add("width", width);
+            Description.Add("price", price);
+            return Description;
         }
     }
 }
