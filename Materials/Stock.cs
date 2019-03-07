51d3a9fa51e8d6ac04525b4e3e0e0c7c6169e670
bernard169 <bernard@tourneur.org>
2019-03-07 12:13:28 +0100

    Stock interface done

--- Materials/Stock.cs
+++ Materials/Stock.cs
@@ -9,5 +9,18 @@ namespace Materials
     class Stock
     {
 
+        
+        public bool isAvailable (Piece piece)
+        {
+            return false;
+        }
+        public void orderPieces (Piece* piece)
+        {
+
+        }
+        public string getPieceDescription (Piece piece)
+        {
+            return "";
+        }
     }
 }
