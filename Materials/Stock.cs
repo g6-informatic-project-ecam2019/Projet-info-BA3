591682bcd7d7a4d32f01ee82aa0b89f96eff7b9c
bernard169 <bernard@tourneur.org>
2019-03-16 20:13:27 +0100

    add existingHeights function

--- Materials/Stock.cs
+++ Materials/Stock.cs
@@ -66,7 +66,7 @@ namespace Materials
         }
 
         /************************************************************************************************* 
-         * Pre : - recieve a Piece as parameter                                                          *
+         * Pre :  recieve a Piece as parameter                                                           *
          * Post : returns the correct name, that matches the field in the database                       *
          *************************************************************************************************/
         private string translateInfos (Piece piece)
@@ -123,11 +123,12 @@ namespace Materials
          *       - the piece is not a Panel, nor a door                                                  *
          * Post : edit the dictionnary with informations found in the DB                                 *
          *************************************************************************************************/
-        private void retrieveInfos (Dictionary <string, Object> dic, Piece piece, string dimension, string color, string code) //dimension is the determining dimension in the DB (height depth or width)
-        {
-            
+        private void retrieveInfos (Dictionary <string, Object> dic, Piece piece, string dimension, string color, string code)  //function only used internally, by getDescription() => private function
+        {                                                                                                                       //dimension is the determining dimension in the DB (height depth or width)
+
             connect();
             int length = (int)piece.GetDescription()["length"];
+            dic.Add(dimension, length);
             if ((piece is Door) || (piece is Panel))
             {
                 throw new System.ArgumentException("The piece cannot be a door nor a panel in this function");
@@ -155,7 +156,7 @@ namespace Materials
             }
             connection.Close();
             connect();
-            this.command.CommandText = String.Format("SELECT * FROM prices WHERE code={0}", code);
+            this.command.CommandText = String.Format("SELECT * FROM prices WHERE code='{0}'", code);
             reader = command.ExecuteReader();
             int i = 0;
             while (reader.Read())
@@ -175,8 +176,10 @@ namespace Materials
 
             connect();
             int length = (int)piece.GetDescription()["length"];
+            dic.Add(dimension1, length);
             int width = (int)piece.GetDescription()["width"];
-            this.command.CommandText = String.Format("SELECT * FROM pieces WHERE ref='{0}'", translateInfos(piece));
+            dic.Add(dimension2, width);
+            command.CommandText = String.Format("SELECT * FROM pieces WHERE ref='{0}'", translateInfos(piece));
             reader = command.ExecuteReader();
             while (reader.Read()) //retrieve all informations about the piece, and put them in the description dictionnary
             {
@@ -188,49 +191,47 @@ namespace Materials
                         {
                             code = reader["code"].ToString();
                             dic.Add("code", code);
-                            dic.Add("client price", (int)reader["client_price"]);
+                            dic.Add("client price", (float)reader["client_price"]);
                         }
                         else if (reader["color"].ToString() == color)
                         {
                             code = reader["code"].ToString();
                             dic.Add("code", code);
-                            dic.Add("client price", (int)reader["client_price"]);
+                            dic.Add("client price", (float)reader["client_price"]);
                         }
                     }  
                 }
             }
             connection.Close();
             connect();
-            this.command.CommandText = String.Format("SELECT * FROM prices WHERE code={0}", code);
+            this.command.CommandText = String.Format("SELECT * FROM prices WHERE code='{0}'", code);
             reader = command.ExecuteReader();
             int i = 0;
             while (reader.Read())
             {
                 i++;
-                dic.Add(String.Format("supplier price {0}", i), (int)reader["supplier_price"]); //add different prices from each suppliers    
+                dic.Add(String.Format("supplier price {0}", i), (float)reader["supplier_price"]); //add different prices from each suppliers    
             }
             dic.Add("n suppliers", i);
             connection.Close();
         }
 
-        /*******************************************************************
-         * Pre : recieve a Piece as paramater                              *
-         * Post : Returns the piece's complete description                 *
-         * Uses the piece characteristics to find the piece in the DB      *
-         *******************************************************************/
+        /*****************************************************************************
+         * Pre : recieve a Piece as paramater                                        *
+         * Post : - Returns the piece's complete description.                        *
+         *          Dictionnary contains the following keys : heigth, depth, width   *
+         *          color, code, client price, supplier price  (there may be more    *
+         *          than one) and n suppliers, the number of suppliers that offer    *
+         *          the piece. The dimension(s) are present or not following the     *
+         *          piece's type                                                     *
+         * Uses the piece characteristics to find the piece in the DB                *
+         *****************************************************************************/
         public Dictionary <string, Object> getPieceDescription (Piece piece)
         {
             
             Dictionary<string, Object> description = new Dictionary<string, Object>();
-            
-            int width;
             string code="";
             string color="";
-            if ((piece is Panel)||(piece is Door))
-            {
-                width = (int)piece.GetDescription()["width"];
-                description.Add("width", width);
-            }
             if ((piece is ClassicDoor) || (piece is Panel) || (piece is Angle))
             {
                 color = (string)piece.GetDescription()["color"];
@@ -238,7 +239,7 @@ namespace Materials
             }
 
             //since the determining dimension in DB is different following the type of piece, we have to make a different criterea for each type
-            if (piece is Angle)
+            if (piece is Angle) //criteria is heigth
             {
                 retrieveInfos(description, piece, "height", color, code);
             }
@@ -258,9 +259,9 @@ namespace Materials
             {
                 retrieveInfos(description, piece, "heigth", color, code);
             }
-            else if (piece is Panel)
+            else if (piece is Panel) 
             {
-                if (piece.GetDescription()["pos"].ToString() == "GD")
+                if (piece.GetDescription()["pos"].ToString() == "GD") //criteria is heigth and depth
                 {
                     retrieveInfos2D(description, piece, "heigth", "depth", color, code);
                 }
@@ -277,14 +278,13 @@ namespace Materials
             {
                 if (piece is GlassDoor)
                 {
-                    retrieveInfos2D(description, piece, "heigth", "width", "Verre", code);
+                    retrieveInfos2D(description, piece, "heigth", "width", "Verre", code); //in the DB, glass door are marked as glass colored
                 }
                 else
                 {
                     retrieveInfos2D(description, piece, "heigth", "width", color, code);
                 }
             }
-
 
             return description;
         }
@@ -293,10 +293,28 @@ namespace Materials
          * Pre : 
          * Post : returns existing dimensions  
          */
-         public int[] existingDimensions()
+         public int[] existingHeights()
          {
-            int[] dimensions = new int[] { 52, 44, 32 };
-            return dimensions;
+            
+            connect();
+            command.CommandText = String.Format("SELECT * FROM pieces WHERE ref='Tasseau'"); //height of a box is given by cleat's length + 4 cm
+            reader = command.ExecuteReader();
+            int counter = 0;
+            while (reader.Read())
+            {
+                counter++;
+            }
+            connection.Close();
+            int[] heights = new int[counter];
+            connect();
+            reader = command.ExecuteReader();
+            counter = 0;
+            while (reader.Read())
+            {
+                heights[counter] = (int) reader["height"];
+                counter++;
+            }
+            return heights;
          }
     }
 }
