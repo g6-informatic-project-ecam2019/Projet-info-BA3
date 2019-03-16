e6bacbdc6c9213fc82551b8b3e84046e73dc5060
bernard169 <bernard@tourneur.org>
2019-03-16 16:32:16 +0100

    add retrieveInfos function

--- Materials/Stock.cs
+++ Materials/Stock.cs
@@ -15,16 +15,13 @@ namespace Materials
         private string connString;
         private MySqlConnection connection;
         private MySqlCommand command;
-        private MySqlCommand command2;
         private MySqlDataReader reader;
-        private MySqlDataReader reader2;
 
         public Stock(string connString)
         {
             this.connString = connString; //"Server=localhost;Port=3306;Database=mykitbox;Uid=root;Pwd="
             connection = new MySqlConnection(this.connString);
             command = connection.CreateCommand();
-            command2 = connection.CreateCommand();
             //command.CommandText = "SELECT * FROM pieces WHERE ref='Porte'"; //this is a "select" command, replace tableName and number
             connect();
             connection.Close();
@@ -115,6 +112,39 @@ namespace Materials
         }
 
         /*----------------------------------------------------------------------------*/
+        private void retrieveInfos (Dictionary <string, Object> dic, Piece piece, string dimension, int length, string color, string code) //dimension is the determining dimension in the DB (height depth or width)
+        {
+            connect();
+            this.command.CommandText = String.Format("SELECT * FROM pieces WHERE ref='{0}'", translateInfos(piece));
+
+            reader = command.ExecuteReader();
+            while (reader.Read()) //retrieve all informations about the piece, and put them in the description dictionnary
+            {
+                if ((int)reader[dimension] == length)
+                {
+                    if (reader["color"].ToString() == color)
+                    {
+                        code = reader["code"].ToString();
+                        dic.Add("code", code);
+                        dic.Add("client price", (int)reader["client_price"]);
+                    }
+                }
+            }
+            connection.Close();
+            connect();
+            this.command.CommandText = String.Format("SELECT * FROM prices WHERE code={0}", code);
+            reader = command.ExecuteReader();
+            int i = 0;
+            while (reader.Read())
+            {
+                i++;
+                dic.Add(String.Format("supplier price {0}", i), (int)reader["supplier_price"]); //add different prices from each suppliers    
+            }
+            dic.Add("n suppliers", i);
+            connection.Close();
+        }
+
+        /*----------------------------------------------------------------------------*/
         public Dictionary <string, Object> getPieceDescription (Piece piece)
         {
             /* Pre : recieve a Piece as paramater
@@ -124,34 +154,36 @@ namespace Materials
             Dictionary<string, Object> description = new Dictionary<string, Object>();
             int length = (int)piece.GetDescription()["length"];
             int width;
+            string code="";
             string color="";
             if ((piece is Panel)||(piece is Door))
-                {
-                    width = (int)piece.GetDescription()["width"];
-                }
+            {
+                width = (int)piece.GetDescription()["width"];
+                description.Add("width", width);
+            }
             if ((piece is ClassicDoor) || (piece is Panel) || (piece is Angle))
-                {
-                    color = (string)piece.GetDescription()["color"];
-                }
-            connect();
+            {
+                color = (string)piece.GetDescription()["color"];
+                description.Add("color", color);
+            }
 
-            //since the determining dimension is different following the type of piece, we have to make a different criterea for each type
+            //since the determining dimension in DB is different following the type of piece, we have to make a different criterea for each type
             if (piece is Angle)
             {
-                this.command.CommandText = String.Format("SELECT * FROM pieces WHERE ref='{0}'", translateInfos(piece));
-                
-                reader = command.ExecuteReader();
-                while (reader.Read()) //retrieve all informations about the piece, and put them in the description dictionnary
+                retrieveInfos(description, piece, "height", length, color, code);
+            }
+
+            if (piece is Breadth)
+            {
+                if ((piece.GetDescription()["pos"].ToString() == "Av") || (piece.GetDescription()["pos"].ToString() == "Ar"))
                 {
-                    if ((int)reader["height"] == length)
-                    {
-                        if (reader["color"].ToString()== color)
-                        {
-                            description.Add("code", reader["code"].ToString());
-                            description.Add("client price", (int) reader["client_price"]);
-                        }
-                    }
+                    retrieveInfos(description, piece, "width", length, color, code);
                 }
+                else
+                {
+                    retrieveInfos(description, piece, "depth", length, color, code);
+                }
+                
             }
             connection.Close();
             return description;
